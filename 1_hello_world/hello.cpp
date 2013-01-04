5ff9a249191cc0ecf0aba291f278cc938db998fe
Zero <john.royer@gmail.com>
2013-01-04 10:18:38 +0800

    Connecting Signal and Slot
    Connect signal clicked() of QPushButton to slot closeAllWindows()
    of QApplication. After that, application exit on button clicked.

--- 1_hello_world/hello.cpp
+++ 1_hello_world/hello.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]){
    QApplication app(argc, argv);
    QPushButton button("Hello World");
 
+   QObject::connect(&button, SIGNAL(clicked()), &app, SLOT(closeAllWindows()) );
+
    button.show();
    app.exec();
    return 0;
